4ca380bccf69f277a425642e29754d35e2b366fe
Florian Geisenhof <florian.geisenhof@bihler.de>
2024-10-15 13:08:06 +0200

    Cleanup

--- rest.py
+++ rest.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, request
+from zwift import Client
+from zrconfig import zwiftuser, zwiftpwd, runalyzeToken
 
 app = Flask(__name__)
+
+client = Client(zwiftuser, zwiftpwd)
+zwiftProfile = client.get_profile()
+zwiftActivities = client.get_activity(zwiftProfile.profile["id"])
+activitiesList = zwiftActivities.list()
 
 # Beispiel-Daten (in der Praxis würdest du wahrscheinlich eine Datenbank verwenden)
 data = {
@@ -11,7 +18,7 @@ data = {
 # GET: Alle Elemente abrufen
 @app.route('/items', methods=['GET'])
 def get_items():
-    return jsonify(data)
+    return jsonify(activitiesList)
 
 # GET: Ein bestimmtes Element abrufen
 @app.route('/items/<int:item_id>', methods=['GET'])
@@ -19,33 +26,6 @@ def get_item(item_id):
     item = data.get(item_id)
     if item:
         return jsonify(item)
-    else:
-        return jsonify({"error": "Item not found"}), 404
-
-# POST: Ein neues Element erstellen
-@app.route('/items', methods=['POST'])
-def create_item():
-    new_id = max(data.keys()) + 1
-    new_item = request.json
-    data[new_id] = new_item
-    return jsonify(new_item), 201
-
-# PUT: Ein vorhandenes Element aktualisieren
-@app.route('/items/<int:item_id>', methods=['PUT'])
-def update_item(item_id):
-    item = data.get(item_id)
-    if item:
-        item.update(request.json)
-        return jsonify(item)
-    else:
-        return jsonify({"error": "Item not found"}), 404
-
-# DELETE: Ein Element löschen
-@app.route('/items/<int:item_id>', methods=['DELETE'])
-def delete_item(item_id):
-    if item_id in data:
-        del data[item_id]
-        return jsonify({"message": "Item deleted"}), 204
     else:
         return jsonify({"error": "Item not found"}), 404
 
