55dbe3f1958cfe45178377465934bdac4f129b63
Florian Geisenhof <florian.geisenhof@gmail.com>
2024-10-15 16:07:11 +0000

    Refactoring

--- rest.py
+++ rest.py
@@ -9,18 +9,18 @@ zwiftProfile = client.get_profile()
 zwiftActivities = client.get_activity(zwiftProfile.profile["id"])
 activitiesList = zwiftActivities.list()
 
-# GET: Alle Elemente abrufen
+
 @app.route('/items', methods=['GET'])
 def get_items():
     return jsonify(activitiesList)
 
-# Beispiel-Daten (in der Praxis würdest du wahrscheinlich eine Datenbank verwenden)
+
 data = {
     1: {"name": "Item 1", "description": "This is item 1"},
     2: {"name": "Item 2", "description": "This is item 2"},
 }
 
-# GET: Ein bestimmtes Element abrufen
+
 @app.route('/items/<int:item_id>', methods=['GET'])
 def get_item(item_id):
     item = data.get(item_id)
@@ -28,6 +28,7 @@ def get_item(item_id):
         return jsonify(item)
     else:
         return jsonify({"error": "Item not found"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
