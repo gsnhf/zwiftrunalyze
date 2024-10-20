770e5e3fa968de1f61664d52060fc00c5b6173a2
Florian Geisenhof <florian.geisenhof@gmail.com>
2024-10-20 18:49:16 +0200

    Changes

--- rest.py
+++ rest.py
@@ -109,6 +109,5 @@ async def transfer_file(activtiy_id):
 
 
 if __name__ == '__main__':
-    # app.run(port=5000)
-    app.run(debug=True)
-# asyncio.run(main())
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
