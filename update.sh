298a2e9d96be039a7aa74efa76a6b48abf313311
gsnhf <florian.geisenhof@gmail.com>
2024-12-14 14:18:25 +0100

    Changed update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "===================="
+echo "Update ZwiftRunalzye"
+echo "===================="
+
 docker-compose down
 git pull
-docker-compose up -d --build
+docker-compose up -d #--build
